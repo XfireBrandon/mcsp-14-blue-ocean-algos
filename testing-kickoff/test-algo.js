9cb2b5d13e9642301a0c2b3dd4a1d183be7272bd
Garrett Ross <garrettross@gGarrettRoss.attlocal.net>
2022-11-04 11:28:38 -0500

    Ransom note algo

---
+++ testing-kickoff/test-algo.js
@@ -0,0 +1,34 @@
+// // function ending(str) {
+// //     const arr = str.split(' ')
+// //     const vowels = ['a', 'e', 'i', 'o', 'u']
+// //     let count = 0
+// //     let current;
+// //     let last;
+
+// //     for(let i = 0; i < arr.length; i++) {
+// //         current = arr[i]
+// //         last = current[current.length - 1].toLowerCase()
+// //         if(vowels.includes(last)) {
+// //             count++
+// //         }
+// //     }
+// //     return count
+// // }
+
+// function ending(str) {
+//     const vowels = ['a', 'e', 'i', 'o', 'u']
+//     let count = 0
+//     str.split(' ').forEach((elem) => {
+//         let last = elem[elem.length - 1].toLowerCase()
+//         if(vowels.includes(last))
+//         count++
+//     })
+//     return count
+// }
+
+// module.exports = ending
+function test() {
+    var x = '1'
+}
+
+console.log(x)
---
+++ testing-kickoff/test-algo.test.js
@@ -0,0 +1,15 @@
+const ending = require('./test-algo')
+
+test('if str is passed in, should return 3', () => {
+    const str = 'I wanna code now'
+    expect(ending(str)).toBe(3)
+})
+
+
+
+
+
+
+
+
+
