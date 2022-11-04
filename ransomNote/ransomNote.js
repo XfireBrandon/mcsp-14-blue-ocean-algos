225f1949776c5d968da398dc8cdc966c74462a10
Garrett Ross <garrettross@gGarrettRoss.attlocal.net>
2022-11-04 12:25:44 -0500

    solution

--- ransomNote/ransomNote.js
+++ ransomNote/ransomNote.js
@@ -1,6 +1,6 @@
 const magazinePage = `This is our fall catolog. We have many different brands of curtains. Low low prices. Customers welcome. Bring your kids, sons and daughters welcome to the store. We will be there early in the evening, so come have fun. It is our business to make sure you get a good deal.`
 
-const note = `We have the daughters. Send the price!`
+const note = 'is our fall'
 
 
 
@@ -8,8 +8,24 @@ function ransomNote(magazinePage, note) {
     const magArr = magazinePage.split(' ')
     const noteArr = note.split(' ')
 
-    console.log(magArr)
-    console.log(noteArr)
+    const magObj = {}
+
+    magArr.forEach((word) => {
+        if(!magObj[word]) magObj[word] = 0
+        magObj[word]++
+    })
+
+    let noteIsPossible = true
+
+    noteArr.forEach((word) => {
+        if(magObj[word]) {
+            magObj[word]--
+            if(magObj[word] < 0) noteIsPossible = false
+        }
+        else 
+        noteIsPossible = false
+    })
+    return noteIsPossible
 }
 
-ransomNote(magazinePage, note)
+console.log(ransomNote(magazinePage, note))
