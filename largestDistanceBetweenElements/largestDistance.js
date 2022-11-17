03a4eea24ad0fb911e10ae07b4ef9c6c7f8567c6
Garrett Ross <garrettross@gGarrettRoss.attlocal.net>
2022-11-17 09:43:06 -0600

    largestDistance added

---
+++ largestDistanceBetweenElements/largestDistance.js
@@ -0,0 +1,11 @@
+// Given an array of elements
+
+//  Ensure that the array is composed on only unique value
+// Find the greatest distance between two values side by side. 
+
+function largestDistance(arr) {
+    // your code here...
+}
+
+const arr = [111,222,333,222,444,333,555,444,667,666,777,888,999]
+console.log(largestDistance())
