fc1b9129756da95112a58efc759288d9c559ae27
Garrett Ross <garrettross@gGarrettRoss.attlocal.net>
2022-11-17 11:25:06 -0600

    LongestDistance added

--- largestDistanceBetweenElements/largestDistance.js
+++ largestDistanceBetweenElements/largestDistance.js
@@ -4,8 +4,18 @@
 // Find the greatest distance between two values side by side. 
 
 function largestDistance(arr) {
-    // your code here...
+    const newArr = [...new Set(arr)]
+    let maxDistance = 0
+    let next, current, currentDistance
+    
+    for(let i = 0; i < newArr.length - 1; i++) {
+        next = newArr[i + 1]
+        current = newArr[i]
+        currentDistance = next - current
+        maxDistance = Math.max(currentDistance, maxDistance)
+    }
+    return maxDistance
 }
 
 const arr = [111,222,333,222,444,333,555,444,667,666,777,888,999]
-console.log(largestDistance())
+console.log(largestDistance(arr))
